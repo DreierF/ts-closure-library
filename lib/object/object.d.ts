294f466953f6093cb725a53dfd94e9e4ec0ca700
Daniel Veihelmann <da.ve@gmx.net>
2020-04-29 10:27:53 +0200

    Another update

--- lib/object/object.d.ts
+++ lib/object/object.d.ts
@@ -276,7 +276,7 @@ export function getValueByKeys(obj: any, ...args: (string | number | ArrayLike<s
  * @return {!Array<V>} The values in the object/map/hash.
  * @template K,V
  */
-export function getValues<T>(obj: { [s: string]: T } | ArrayLike<T>): T[];
+export function getValues<T = unknown>(obj: Record<string, T> | ArrayLike<T> | object): T[];
 /**
  * @fileoverview Utilities for manipulating objects/maps/hashes.
  */
